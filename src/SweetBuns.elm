d9d6556fd73ed53f0c471946a47b85be689f24aa
Jakob Eklund <jakob.eklund@superattack.se>
2018-12-25 20:12:02 +0100

    Highlight possible movement destinations

--- src/SweetBuns.elm
+++ src/SweetBuns.elm
@@ -66,9 +66,14 @@ type FloorTile
 -}
 type alias RenderableTile =
     { content : Maybe Thingy
-    , highlight : Bool
+    , highlight : Maybe Highlight
     , floor : FloorTile
     }
+
+
+type Highlight
+    = SelectedMover
+    | PossibleMovementDestination
 
 
 
@@ -574,15 +579,9 @@ viewBoard model =
                 |> Grid.translate ( 0, 1 - boardSize.height )
                 |> Dict.intersect kitchenLevel
                 -- Convert to full tiles
-                |> Dict.map (\_ floorTile -> RenderableTile Nothing False floorTile)
-                -- Render highlighed column
-                |> Dict.map
-                    (\coords tile ->
-                        if (Just coords == model.selectedTile) then
-                            { tile | highlight = True }
-                        else
-                            tile
-                    )
+                |> Dict.map (\_ floorTile -> RenderableTile Nothing Nothing floorTile)
+                -- Render highlighed tiles
+                |> Dict.map (\tileCoords tile -> { tile | highlight = getPossibleHighlight model tileCoords })
                 -- Render buns
                 |> Dict.map
                     (\coords tile ->
@@ -592,6 +591,23 @@ viewBoard model =
                     )
     in
         Grid.toHtmlDiv ( tileSide, tileSide ) renderTile finalGrid
+
+
+{-| Determine if the tile at the given coords should be highlighted in some way.
+-}
+getPossibleHighlight : Model -> Coords -> Maybe Highlight
+getPossibleHighlight { selectedTile } tileCoords =
+    Maybe.map
+        (\selectedCoords ->
+            if (tileCoords == selectedCoords) then
+                Just SelectedMover
+            else if (isValidMove selectedCoords tileCoords) then
+                Just PossibleMovementDestination
+            else
+                Nothing
+        )
+        selectedTile
+        |> Maybe.withDefault (Nothing)
 
 
 viewInfo : Maybe Thingy -> Html msg
@@ -689,22 +705,25 @@ renderTile coords tile =
 
 
 getTileColor : RenderableTile -> String
-getTileColor tile =
-    if tile.highlight then
-        "aqua"
-    else
-        case tile.floor of
-            Spawner _ ->
-                "antiquewhite"
+getTileColor { highlight, floor } =
+    case ( highlight, floor ) of
+        ( Just SelectedMover, _ ) ->
+            "darkblue"
 
-            PlainTile ->
-                "lemonchiffon"
+        ( Just PossibleMovementDestination, _ ) ->
+            "aliceblue"
 
-            BunCollector ->
-                "yellowgreen"
+        ( Nothing, Spawner _ ) ->
+            "antiquewhite"
 
-            WallTile ->
-                "darkgray"
+        ( Nothing, PlainTile ) ->
+            "lemonchiffon"
+
+        ( Nothing, BunCollector ) ->
+            "yellowgreen"
+
+        ( Nothing, WallTile ) ->
+            "darkgray"
 
 
 px : ( String, Int ) -> ( String, String )
